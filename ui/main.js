8224f66a75e5c7cce396dae082db4ceee74f8ff5
KAKARAPARTHI SIVA VARA PRASAD <sivavaraprasadkakaraparthi@gmail.com>
2018-02-21 21:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,9 @@ var element = document.getElementById("text");
 var image = document.getElementById("madi");
 
 element.innerHTML="NEW VALUE";
+var marginRight = 10;
 function moveLeft(){
-    var marginRight = 10;
+    
     image.style.marginRight = marginRight + "px";
 }
 
