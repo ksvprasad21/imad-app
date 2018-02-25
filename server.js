e23a92ef0b39e8a57efdaa256115e94b578d4e13
KAKARAPARTHI SIVA VARA PRASAD <sivavaraprasadkakaraparthi@gmail.com>
2018-02-25 13:39:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('ui/article.html', function (req,res) {
+app.get('/ui/article.html', function (req,res) {
     res.sendFile(path.join(_dirname,'ui','article.html'));
 });
 
