8add516d91c7e0970a4a7745c23f95b9faadf726
KAKARAPARTHI SIVA VARA PRASAD <sivavaraprasadkakaraparthi@gmail.com>
2018-02-25 12:04:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,11 +2,22 @@ console.log('Loaded!');
 
 var buton = document.getElementById("counter");
 
-var counter=0;
+
 
 buton.onclick=function(){
+    
+    var request = new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readyState=== XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+                counter+=1;
+                var span =document.getElementById("count");
+                span.innerHTML=counter.toString(); 
+            }
+        }
+    };
     //render counter value in correct span
-    counter+=1;
-    var span =document.getElementById("count");
-    span.innerHTML=counter.toString();
+   request.open('GET',"http://sivavaraprasad97.imad.hasura-app.io/counter",'true');
+   request.send(null);
 };
