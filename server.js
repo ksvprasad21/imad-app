5b15c2a7caa16df8765bed7547703f223d3a5fb7
KAKARAPARTHI SIVA VARA PRASAD <sivavaraprasadkakaraparthi@gmail.com>
2018-02-25 15:11:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('/counter',function (req, res){
+    counter =counter + 1;
+    res.send(counter.toString());
+});
+
 var articles ={
     'article-one':{
         title:'Article',
@@ -120,10 +125,7 @@ app.get('/ui/main.js',function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var counter=0;
-app.get('/counter',function (req, res){
-    counter =counter + 1;
-    res.send(counter.toString());
-});
+
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
