2b9cb5aa7fe16ce0a7f502a48f3d87119dfd4a3e
KAKARAPARTHI SIVA VARA PRASAD <sivavaraprasadkakaraparthi@gmail.com>
2018-02-25 11:42:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,12 @@
 console.log('Loaded!');
 
+var buton = document.getElementById("counter");
+
+var counter=0;
+
+buton.onclick=function(){
+    //render counter value in correct span
+    counter+=1;
+    var span =document.getElemntById("count");
+    span.innerHTML=counter.toString();
+};
